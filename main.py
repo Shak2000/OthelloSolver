7361e332f34c2e2ad3804d4660d2d2fe8d477c8e
Shak Ragoler <shak.ragoler@gmail.com>
2025-07-17 09:58:31 -0700

    Improving scoring mechanisms

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ class Game:
         self.history = []
         self.player = 'B'
         self.rem = 0
+        self.black = 0
+        self.white = 0
 
     def start(self):
         self.board = [['.' for j in range(8)] for i in range(8)]
@@ -15,10 +17,6 @@ class Game:
         if 0 <= x < 8 and 0 <= y < 8 and self.board[y][x] == '.':
             self.board[y][x] = self.player
             self.rem -= 1
-            if self.player == 'B':
-                self.player = 'W'
-            else:
-                self.player = 'B'
 
             self.flip(x, y, 0, 1)
             self.flip(x, y, 0, -1)
@@ -29,6 +27,12 @@ class Game:
             self.flip(x, y, -1, 1)
             self.flip(x, y, -1, -1)
 
+            if self.player == 'B':
+                self.black += 1
+                self.player = 'W'
+            else:
+                self.white += 1
+                self.player = 'B'
             self.history.append([[self.board[i][j] for j in range(8)] for i in range(8)])
             return True
 
@@ -48,25 +52,18 @@ class Game:
                 ex -= dx
                 ey -= dy
                 self.board[ey][ex] = self.player
+                if self.player == 'B':
+                    self.black += 1
+                else:
+                    self.white += 1
 
     def get_winner(self):
         if self.rem > 0:
-            b = 0
-            w = 0
-
-            for i in range(8):
-                for j in range(8):
-                    if self.board[i][j] == 'B':
-                        b += 1
-                    elif self.board[i][j] == 'W':
-                        w += 1
-
-            if b > w:
+            if self.black > self.white:
                 return 'B'
-            elif w > b:
+            elif self.white > self.black:
                 return 'W'
             return 'T'
-
         return None
 
     def undo(self):
